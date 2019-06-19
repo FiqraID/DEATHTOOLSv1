f66610855f7e2c417c587565413d998dc76610cc
FiqraID <51190920+FiqraID@users.noreply.github.com>
2019-06-20 05:57:12 +0700

    Add files via upload

---
+++ death.sh
@@ -0,0 +1,521 @@
+blue='\033[34;1m' #biru
+green='\033[32;1m' #ijo
+purple='\033[35;1m' #purple
+cyan='\033[36;1m' #cyan
+red='\033[31;1m' #merah
+white='\033[37;1m' #putih
+yellow='\033[33;1m' #kuning
+orange='\033[1;38;5;208m' #Orange
+echo
+
+clear
+echo $orange "============================================="
+figlet -f pagga "Mr.GlitchCyber" | lolcat
+echo $orange "============================================="
+
+echo $cyan "{+}----------------------------------------{+}"
+echo "  | create by:Mr.GlitchCyber#07 and Mr.F1QR4 | "
+echo "  | versi    :1.0.0                          | "
+echo "  | penerbit :Mr.GlitchCyber                 | "
+echo "  | associate:DUNIA HACKER INDONESIA         | "
+echo $cyan "{+}----------------------------------------{+}"
+echo
+
+figlet -f pagga "Mr.F1QR4" |lolcat
+
+
+echo "MENU TOOLS" | lolcat
+
+echo "   [•]---------------------------------------------[•]" | lolcat
+echo " 1. | LITESPAM                                      |" | lolcat
+echo " 2. | dark fb tanpa lisensi                         |" | lolcat
+echo " 3. | hack fb target part I                         |" | lolcat
+echo " 4. | LITEDDOS                                      |" | lolcat
+echo " 5. | PDF-TOOLS                                     |" | lolcat
+echo " 6  | termux-key                                    |" | lolcat
+echo " 7. | Sadap Whatsapp                                |" | lolcat
+echo " 8. | RED_HAWK                                      |" | lolcat
+echo " 9. | katoolin                                      |" | lolcat
+echo "10. | Spamcall                                      |" | lolcat
+echo "11. | ip_Attack                                     |" | lolcat
+echo "12. | Malicious                                     |" | lolcat
+echo "13. | Localizer IP                                  |" | lolcat
+echo "14. | Torshammer                                    |" | lolcat
+echo "15. | Xerxes                                        |" | lolcat
+echo "16. | webdav                                        |" | lolcat
+echo "17. | crewbot                                       |" | lolcat
+echo "18. | SQLMAP                                        |" | lolcat
+echo "19. | HAKKU                                         |" | lolcat
+echo "20. | D-TECT                                        |" | lolcat
+echo "21. | Lazymux                                       |" | lolcat
+echo "22. | Hash buster                                   |" | lolcat
+echo "23. | ubuntu                                        |" | lolcat
+echo "24. | RECONDOG                                      |" | lolcat
+echo "25. | Deface                                        |" | lolcat
+echo "26. | pulsa gratis                                  |" | lolcat
+echo "27. | wifi-hacker                                   |" | lolcat
+echo "28. | moon-buggy                                    |" | lolcat
+echo "29. | tembak quota                                  |" | lolcat
+echo "30. | mpsyt                                         |" | lolcat
+echo "31. | OSIF                                          |" | lolcat
+echo "32. | Spammer Grab                                  |" | lolcat
+echo "33. | santet-online                                 |" | lolcat
+echo "34. | BOOT FACEBOOK                                 |" | lolcat
+echo "35. | Mbf                                           |" | lolcat
+echo "36. | BRUTEFORCE                                    |" | lolcat
+echo "37. | Weeman                                        |" | lolcat
+echo "38. | Botkomena                                     |" | lolcat
+echo "39. | mr.c4kil                                      |" | lolcat
+echo "40. | LITESCRIPT                                    |" | lolcat
+echo "41. | YahooCloning                                  |" | lolcat
+echo "42. | Unix                                          |" | lolcat
+echo "43. | wifite <root>                                 |" | lolcat
+echo "44. | sms gratis                                    |" | lolcat
+echo "45. | fb-hack                                       |" | lolcat
+echo "46. | GERETONGERS                                   |" | lolcat
+echo "47. | Phising                                       |" | lolcat
+echo "48. | ubh quota vidmax jdi flash                    |" | lolcat
+echo "49. | nuyul Gopay                                   |" | lolcat
+echo "50. | pulsafree                                     |" | lolcat
+echo "00. | keluar                                        |" | lolcat
+echo "   [•]---------------------------------------------[•]" | lolcat
+echo $purple"┌==="$blue"["$cyan"DEATHTOOLS"$blua"]"$red"======"$yellow"["$cyan""MASUKAN PILIHAN MU""$purple"]"
+echo $me"¦"
+read -p"└──# " noob
+
+
+
+if [ $noob = 1  ] || [ $noob = 1 ]
+then
+clear
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+
+if [ $noob = 2 ] || [ $noob = 2 ]
+then
+clear
+git clone https://github.com/darkfb/terbaru
+cd terbaru
+python2 dark.py
+fi
+
+if [ $noob = 3 ] || [ $noob = 3 ]
+then
+clear
+git clone https://github.com/sumar-kun/hackfb
+cd hackfb
+unzip hack.sh
+sh hack.sh
+fi
+
+if [ $noob = 4 ] || [ $noob = 4 ]
+then
+clear
+git clone https://github.com/4L13199/LITEDDOS
+cd LITEDDOS
+python2 LITEDDOS.py
+fi
+
+if [ $noob = 5 ] || [ $noob = 5 ]
+then
+clear
+git clone https://github.com/FiqraID/PDF-TOOLS
+cd PDF-TOOLS
+sh tools.sh
+fi
+
+if [ $noob = 6 ] || [ $noob = 6 ]
+then
+clear
+git clone https://FiqraID/termux-key
+cd termux-key
+python2 KeyPlus.py
+fi
+
+if [ $noob = 7 ] || [ $noob = 7 ]
+then
+clear
+git clone https://github.com/MrTamfanX/MrSadapWhatsApp
+cd MrSadapWhatsApp
+chmod +x *
+sh MrSadapWhatsApp.sh
+fi
+
+if [ $noob = 8 ] || [ $noob = 8 ]
+then
+clear
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+chmod +x *
+pkg install php
+php rhawk.php
+fi
+
+if [ $noob = 9 ] || [ $noob = 9 ]
+then
+clear
+pkg install gnupg
+git clone https://github.com/LionSec/katoolin.git
+cd katoolin
+python2 katoolin.py
+nano katoolin.py
+fi
+
+if [ $noob = 10 ] || [ $noob = 10 ]
+then
+clear
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+
+if [ $noob = 11 ] || [ $noob = 11 ]
+then
+clear
+git clone https://github.com/Bhai4You/Ip-Attack
+cd Ip-Attack
+chmod +x *
+python2 ip-attack.py
+fi
+
+if [ $noob = 12 ] || [ $noob = 12 ]
+then
+clear
+
+
+if [ $noob = 13 ] || [ $noob = 13 ]
+then
+clear
+https://github.com/maldevel/IPGeoLocation.git
+cd IPGeoLocation
+pip install -r requirements.txt
+python ipgeolocation.py -m
+python ipgeolocation.py -t http://www.google.com
+fi
+
+if [ $noob = 14 ] || [ $noob = 14 ]
+then
+clear
+git clone https://github.com/dotfighter/torshammer.git 
+cd torshammer 
+python2 torshammer.py
+fi
+
+if [ $noob = 15 ] || [ $noob = 15 ]
+then
+clear
+git clone https://github.com/zanyarjamal/xerxes
+cd xerxes
+clang xerxes.c -o xerxes
+/xerxes (nama website) 80
+fi
+
+if [ $noob = 16 ] || [ $noob = 16 ]
+then
+clear
+git clone https://github.com/muhammadfathul/FBH
+cd FBH
+chmod +x *
+bash setup
+bash FBH
+fi
+
+if [ $noob = 17 ] || [ $noob = 17 ]
+then
+clear
+git clone https://github.com/Xeit666h05t/CrewBot
+cd CrewBot
+python2 CrewBot.pyc
+fi
+
+if [ $noob = 18 ] || [ $noob = 18 ]
+then
+clear
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+Python2 sqlmap.py
+fi
+
+if [ $noob = 19 ] || [ $noob = 19 ]
+then
+clear
+https://github.com/4shadoww/hakkuframework
+cd hakkuframework
+chmod +x hakku
+python hakku.py
+fi
+
+if [ $noob = 20 ] || [ $noob = 20 ]
+then
+clear
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+cd D-TECT
+chmod +x *
+python2 d-tect.py
+fi
+
+if [ $noob = 21 ] || [ $noob = 21 ]
+then
+clear
+git clone https://github.com/Gameye98/Lazymux.git
+cd Lazymux
+python2 lazymux.py
+fi
+
+if [ $noob = 22 ] || [ $noob = 22 ]
+then
+clear
+git clone https://github.com/UltimateHackers/Hash-Buster
+cd Hash-Buster
+python2 hash.py
+fi
+
+if [ $noob = 23 ] || [ $noob = 23 ]
+then
+clear
+git clone https://github.com/Neo￾Oli/termux-ubuntu
+cd termux-ubuntu
+chmod +x *
+./ubuntu.sh
+./start.sh
+fi
+
+if [ $noob = 24 ] || [ $noob = 24 ]
+then
+clear
+https://github.com/UltimateHackers/ReconDog
+cd ReconDog
+chmod +x dog.py
+Python2 dog.py
+fi
+
+if [ $noob = 25 ] || [ $noob = 25 ]
+then
+clear
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+chmod +x *
+python2 create.py
+fi
+
+if [ $noob = 26 ] || [ $noob = 26 ]
+then
+clear
+git clone https://github.com/caker1402/pulsagratis
+cd pulsagratis
+sh pulsa.sh
+fi
+
+if [ $noob = 27 ] || [ $noob = 27 ]
+then
+clear
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x wifi-hacker.sh
+./wifi-hacker.sh
+fi
+
+if [ $noob = 28 ] || [ $noob = 28 ]
+then
+clear
+pkg install moon-buggy
+moon-buggy
+fi
+
+if [ $noob = 29 ] || [ $noob = 29 ]
+then
+clear
+git clone https://github.com/Bintang73/tembaktembakan.git
+cd tembaktembakan
+python dor.py
+fi
+
+if [ $noob = 30 ] || [ $noob = 30 ]
+then
+clear
+pip install mps_youtube
+pip install youtube_dl
+pkg install mpv
+mpsyt
+fi
+
+if [ $noob = 31 ] || [ $noob = 31 ]
+then
+clear
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip install -r requirements.txt
+python2 osif.py
+fi
+
+if [ $noob = 32 ] || [ $noob = 32 ]
+then
+clear
+git clone http://github.com/p4kl0nc4t/Spammer-Grab
+cd Spammer-Grab
+python2 spammer.py
+fi
+
+if [ $noob = 33 ] || [ $noob = 33 ]
+then
+clear
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+fi
+
+if [ $noob = 34 ] || [ $noob = 34 ]
+then
+clear
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $noob = 35 ] || [ $noob = 35 ]
+then
+clear
+git clone https://github.com/pirmansx/mbf
+cd mbf
+pip2 install mechanize
+python2 MBF.py
+fi
+
+if [ $noob = 36 ] || [ $noob = 36 ]
+then
+clear
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $noob = 37 ] || [ $noob = 37 ]
+then
+clear
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+python2 weeman.py
+fi
+
+if [ $noob = 38 ] || [ $noob = 38 ]
+then
+clear
+git clone https://github.com/Senitopeng/Botkomena.git
+cd Botkomena
+python2 botkomena.py
+fi
+
+if [ $noob = 39 ] || [ $noob = 39 ]
+then
+clear
+git clone https://github.com/mrcakil/Mrcakil.git
+cd Mrcakil
+chmod +x tools
+./tools
+fi
+
+if [ $noob = 40 ] || [ $noob = 40 ]
+then
+clear
+git clone https://github.com/4L13199/LITESCRIPT
+cd LITESCRIPT
+python2 LITESCRIPT.py
+fi
+
+if [ $noob = 41 ] || [ $noob = 41 ]
+then
+clear
+git clone https://github.com/wahyuandhika/YahooCloning
+cd YahooCloning
+python2 cloning.py
+fi
+
+if [ $noob = 42 ] || [ $noob = 42 ]
+then
+clear
+git clone https://github.com/InYourG00D1/Unix
+cd Unix
+sh KD-FB.sh
+fi
+
+if [ $noob = 43 ] || [ $noob = 43 ]
+then
+clear
+git clone https://github.com/derv82/wifite
+cd wifite
+python2 wifite.py
+pkg install tsu
+tsu
+python2 wifite.py
+fi
+
+if [ $noob = 44 ] || [ $noob = 44 ]
+then
+clear
+git clone https://github.com/Rusmana-ID/gratis-sms
+cd gratis-sms
+python2 gratis-sms.py
+fi
+
+if [ $noob = 45 ] || [ $noob = 45 ]
+then
+clear
+git clone https://github.com/FR13ND8/fb-hack
+cd fb-hack
+python2 fb-hack.py
+fi
+
+if [ $noob = 46 ] || [ $noob = 46 ]
+then
+clear
+git clone https://github.com/Rusmana-ID/Geretongers
+cd Geretongers
+sh geretongers.sh
+fi
+
+if [ $noob = 47 ] || [ $noob = 47 ]
+then
+clear
+git clone https://github.com/InYourG00D1/ScPh1s1ng
+cd ScPh1s1ng
+sh 6.sh
+fi
+
+if [ $noob = 48 ] || [ $noob = 48 ]
+then
+clear
+git clone https://github.com/InYourG00D1/Flash
+cd Flash
+php flash.php
+fi
+
+if [ $noob = 49 ] || [ $noob = 49 ]
+then
+clear
+git clone https://github.com/InYourG00D1/Nuyul-GoPay
+cd Nuyul-GoPay
+php Go-Pay.php
+fi
+
+if [ $noob = 50 ] || [ $noob = 50 ]
+then
+clear
+git clone https://github.com/InYourG00D1/PulsaFree
+cd PulsaFree
+sh free.sh
+fi
+
+if [ $noob = 51 ] || [ $noob = 51 ]
+then
+clear
+ping 8.8.8.8
+fi
+
+if [ $noob = 00 ] || [ $noob = 00 ]
+then
+exit
+fi
+
+
